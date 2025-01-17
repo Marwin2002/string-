6875b57bbc2d08dba55aacc96e4fec50eb6f7ceb
Marwin2002 <musicbotid@gmail.com>
2025-01-17 07:07:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = 12380656
+API_HASH = "d927c13beaaf5110f25c505b7c071273"
 
-BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = "7944175084:AAFNDr6fIKd4t__0Nj0yrMtrdXe2vY_FrHs"
 #OWNER_ID = int(getenv("8060797897"))
 OWNER_ID = int(getenv("OWNER_ID", "7224419362"))
-MONGO_DB_URI = getenv("MONGO_DB_URI")
-MUST_JOIN = getenv("MUST_JOIN", None)
+MONGO_DB_URI = "mongodb+srv://architect2002:architect2002@cluster0.ccinu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MUST_JOIN = getenv("MUST_JOIN", "The_Architect04")
