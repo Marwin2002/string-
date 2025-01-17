35eca337d6b9a4fc3250ec8bee10056f2284a157
Marwin2002 <musicbotid@gmail.com>
2025-01-17 06:31:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ API_HASH = getenv("API_HASH")
 
 BOT_TOKEN = getenv("BOT_TOKEN")
 #OWNER_ID = int(getenv("8060797897"))
-OWNER_ID = int(getenv("OWNER_ID", "8060797897"))
+OWNER_ID = int(getenv("OWNER_ID", "7224419362"))
 MONGO_DB_URI = getenv("MONGO_DB_URI")
 MUST_JOIN = getenv("MUST_JOIN", None)
