f9f69291557709cde9de086aed8c3efcdb1ed960
Marwin2002 <musicbotid@gmail.com>
2025-01-17 06:56:48 +0530

    Update Callbacks.py

--- String/Callbacks.py
+++ String/Callbacks.py
@@ -1,7 +1,7 @@
 import traceback
 from pyrogram import Client, filters
 from pyrogram.types import CallbackQuery, InlineKeyboardMarkup
-from StringGenBot.generate import generate_session, ask_ques, buttons_ques
+from String.generate import generate_session, ask_ques, buttons_ques
 
 ERROR_MESSAGE = """ɪғ ʏᴏᴜ ᴀʀᴇ ɢᴇᴛᴛɪɴɢ ᴇʀʀᴏʀ!
 ʏᴏᴜ ʜᴀᴠᴇ ᴅᴏɴᴇ sᴏᴍᴇ ᴍɪsᴛᴀᴋᴇ ᴡʜɪʟᴇ ɢᴇɴᴇʀᴀᴛɪɴɢ.
